91e4472d9db573a1bc592b40fac89f752f407d95
wadanaoumi <wada121400@gmail.com>
2021-03-28 23:46:58 +0900

    コメントついｋ

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -18,9 +18,19 @@ public class ProfileTest {
       Profile profile = new Profile("Bull Hockey, Inc.");
       // 質問生成
       Question question = new BooleanQuestion(1, "ボーナスは支給されますか?");
+      // 質問の回答を生成
+      Answer profileAnswer = new Answer(question, Bool.TRUE);
+      // 質問の回答をプロフィールに追加
+      profile.add(profileAnswer);
+
+      // 企業の条件一覧生成
       Criteria criteria = new Criteria();
+      // 質問の回答生成
       Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+      // 条件の重要性
       Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+      // 条件の重要性を一覧に追加
       criteria.add(criterion);
    }
+
 }
