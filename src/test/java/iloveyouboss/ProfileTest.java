4a6c8514c46ad32d2564672d349cd9c7b70f86aa
wadanaoumi <wada121400@gmail.com>
2021-03-29 00:17:13 +0900

    2章終わった

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -8,29 +8,65 @@
 ***/
 package iloveyouboss;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.*;
 
 public class ProfileTest {
+   // プロフィール
+   Profile profile;
+   // 質問
+   Question question;
+   // 企業の条件一覧
+   Criteria criteria;
 
-   @Test
-   public void test() {
+   @Before
+   public void create() {
       // プロフィール生成
-      Profile profile = new Profile("Bull Hockey, Inc.");
+      profile = new Profile("Bull Hockey, Inc.");
       // 質問生成
-      Question question = new BooleanQuestion(1, "ボーナスは支給されますか?");
-      // 質問の回答を生成
-      Answer profileAnswer = new Answer(question, Bool.TRUE);
-      // 質問の回答をプロフィールに追加
-      profile.add(profileAnswer);
+      question = new BooleanQuestion(1, "ボーナスは支給されますか?");
 
       // 企業の条件一覧生成
-      Criteria criteria = new Criteria();
+      criteria = new Criteria();
+   }
+
+   @Test
+   public void matchAnswersFalseWhenMustCriterNotMet() {
+      // 質問の回答をプロフィールに追加
+      profile.add(new Answer(question, Bool.FALSE));
+
       // 質問の回答生成
       Answer criteriaAnswer = new Answer(question, Bool.TRUE);
       // 条件の重要性
       Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
       // 条件の重要性を一覧に追加
       criteria.add(criterion);
+
+      // マッチング開始
+      boolean matches = profile.matches(criteria);
+
+      // マッチングはされなかったか？
+      assertFalse(matches);
    }
 
+   @Test
+   public void matchAnswersTrueForAnyDontCareCriter() {
+      // 質問の回答をプロフィールに追加
+      profile.add(new Answer(question, Bool.TRUE));
+
+      // 質問の回答生成
+      Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+      // 条件の重要性
+      Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+      // 条件の重要性を一覧に追加
+      criteria.add(criterion);
+
+      // マッチング開始
+      boolean matches = profile.matches(criteria);
+
+      // マッチングはしたか？
+      assertTrue(matches);
+   }
 }
